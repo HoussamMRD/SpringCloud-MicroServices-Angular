5cafb913ad492ab930a316d055435bcafeccd491
HoussamMRD <102621669+HoussamMRD@users.noreply.github.com>
2024-01-02 08:38:17 +0100

    config-service

---
+++ config-service/src/test/java/com/MRD/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.MRD.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
